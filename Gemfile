80dffdaff2538dcff2e0442539686c4be7499bea
Yuet Fu Tse <yuettse@hotmail.com>
2019-06-08 21:58:09 -0700

    update

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'devise', '~> 4.6', '>= 4.6.2'
 end
 
 group :test do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
+  	before_action :set_post, only: [:show, :edit, :update, :destroy]
+  	before_action :authenticate_user!
+
 	def index
 		@posts = Post.all.order("created_at DESC")
 	end
 
 	def new
+		@post = current_user.posts.build
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = current_user.posts.build(post_params)
 		@post.save
 
 		redirect_to @post
@@ -16,6 +20,31 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+
+
+	def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def update
+    	respond_to do |format|
+     		 if @post.update(post_params)
+		        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+		        format.json { render :show, status: :ok, location: @post }
+    		 else
+		        format.html { render :edit }
+		        format.json { render json: @post.errors, status: :unprocessable_entity }
+		    end
+    	end
+  	end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
 	private
 		def post_params

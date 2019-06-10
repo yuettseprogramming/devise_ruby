f61d00a545fa7b7b551657898b34fbb40e29029a
Yuet Fu Tse <yuettse@hotmail.com>
2019-06-09 23:28:12 -0700

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   	before_action :set_post, only: [:show, :edit, :update, :destroy]
-  	before_action :authenticate_user!
 
 	def index
 		@posts = Post.all.order("created_at DESC")
